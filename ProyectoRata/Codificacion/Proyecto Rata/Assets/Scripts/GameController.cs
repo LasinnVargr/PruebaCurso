db3e489354185394b228c495eaf5c804c6ed51af
IvanPerezLopez <34062341+IvanPerezLopez@users.noreply.github.com>
2022-12-09 10:57:41 +0100

    Desarrollo de los elementos del personaje

--- ProyectoRata/Codificacion/Proyecto Rata/Assets/Scripts/GameController.cs
+++ ProyectoRata/Codificacion/Proyecto Rata/Assets/Scripts/GameController.cs
@@ -15,4 +15,16 @@ public class GameController : MonoBehaviour
     {
         
     }
+
+    /// <summary>
+    /// Gestión de la muerte del personaje y reaparecer del mismo
+    /// </summary>
+    /// <param name="personaje">Posición del personaje</param>
+    /// <param name="origen">Origen en el que va a reaparecer el personaje</param>
+    public void Reaparecer(Transform personaje, Vector3 origen)
+    {
+        personaje.gameObject.GetComponent<CharacterController>().enabled = false;
+        personaje.position = origen;
+        personaje.gameObject.GetComponent<CharacterController>().enabled = true;
+    }
 }
--- ProyectoRata/Codificacion/Proyecto Rata/Assets/Scripts/Rata.cs
+++ ProyectoRata/Codificacion/Proyecto Rata/Assets/Scripts/Rata.cs
@@ -7,20 +7,27 @@ public class Rata : MonoBehaviour
     Vector3 gravedad = Vector3.zero;
     Vector3 salto = Vector3.zero;
 
+    Vector3 origen;
+
     //Velocidad del personaje
     [SerializeField] float velocidad = 2;
 
     //Aceleración del salto
     [SerializeField] float aceleracionSalto = 10;
 
-
     //Se sustituye el Collider original de la cápsula, por Charcter Controller 
     CharacterController characterController;
+
+    GameController gameController;
 
     // Start is called before the first frame update
     void Start()
     {
+        gameController = GameObject.FindWithTag("GameController").GetComponent<GameController>();
+
         characterController = GetComponent<CharacterController>();
+
+        origen = characterController.transform.position;
     }
 
     // Update is called once per frame
@@ -29,6 +36,9 @@ public class Rata : MonoBehaviour
         //Gestión del salto del personaje
         if (characterController.isGrounded)
         {
+            gravedad.y = -0.01f;
+            salto.y = 0;
+
             if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow) || Input.GetButton("Fire1"))
             {
                 salto.y = aceleracionSalto;
@@ -36,7 +46,7 @@ public class Rata : MonoBehaviour
         }
         else
         {
-            gravedad.y -= .1f;
+            gravedad.y -= .2f;
         }
 
         //Asignación del valor x al movimiento, en función de los cursores o del gamepad
@@ -45,5 +55,14 @@ public class Rata : MonoBehaviour
 
         //Movimiento del personaje
         characterController.Move((gravedad + movimiento + salto) * Time.deltaTime);
+    }
+
+    /// <summary>
+    /// Muerte del personaje
+    /// </summary>
+    public void Muerte()
+    {
+        gameObject.transform.position = origen;
+        gameController.Reaparecer(gameObject.transform, origen);
     }
 }
